b0dc97af330183c91d5bb36b8440f80937f9f7b9
MichaelSimmons2004 <120233805+MichaelSimmons2004@users.noreply.github.com>
2023-10-27 00:06:05 -0500

    File Reworks
    Complete overhaul of existing code

--- PiPicoMain.py
+++ PiPicoMain.py
@@ -1,47 +1,18 @@
-from machine import I2C, PIN
-import time
+from imu import MPU6050
+from time import sleep
+from machine import Pin, I2C
 
-# MPU6050 i2c address: 0x68
-# BMP180 i2c address: 0x77
-
-SDA = Pin(16)
-SCL = Pin(17)
-i2c = I2C(id=0, SDA, SCL)
-
-# Create file
-file = open('Data.txt', 'w')
-file.write("Readings from MPU6050 and BMP180", 'a')
-
+i2c = I2C(1, sda=Pin(2), scl=Pin(3))
+imu = MPU6050(i2c)
 
 while True:
-    # timer
-    startTime = time.time()
-    
-    
     # MPU6050 readings
-    x_accel = i2c.readfrom_mem(0x68, 0x3B, 2)
-    y_accel = i2c.readfrom_mem(0x68, 0x3C, 2)
-    z_accel = i2c.readfrom_mem(0x68, 0x3D, 2)
-    x_ang_vel = i2c.readfrom_mem(0x68, 0x43, 2)
-    y_ang_vel = i2c.readfrom_mem(0x68, 0x45, 2)
-    z_ang_vel = i2c.readfrom_mem(0x68, 0x47, 2)
-    temperature = i2c.readfrom_mem(0x68, 0x41, 2)
-
-
-    # BMP180 readings
-    pressure = i2c.readfrom_mem(0x77, 0xF7, 2)
-    
-    
-    # timer continued
-    endTime = time.time()
-    timeOfReading = endTime - startTime
-    formatted_timeOfReading = f'{timeOfReading:07.3f}'
-    
-    
-    # Add to file
-    values = [x_accel, y_accel, z_accel, x_ang_vel, y_ang_vel, z_ang_vel]
-    formatted_values = [f'{value:010.6f}' for value in values]
-    data = "{}: {}".format(formatted_timeOfReading, formatted_values)
-    file.write(data, 'a')
-    file.close()
-    
+    x_accel = imu.accel.x
+    y_accel = imu.accel.y
+    z_accel = imu.accel.z
+    x_ang_vel = imu.gyro.x
+    y_ang_vel = imu.gyro.y
+    z_ang_vel = imu.gyro.z
+    temperature = imu.temperature
+    print(x_accel, y_ang_vel, temperature)
+    sleep(0.2)
